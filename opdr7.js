3f23f8a9e7f8ae194928c2073a598cb00c3e5488
YSNSVC <89729328+YSNSVC@users.noreply.github.com>
2021-11-14 15:07:19 +0100

    Add files via upload

---
+++ opdr7.js
@@ -0,0 +1,24 @@
+const add = function () {
+    const sum = number1 + number2;
+    return sum;
+};
+return true;
+
+};
+
+const number1 = 50;
+console.log(result);
+
+
+const doubleArray = function (numbers) {
+    const doubled = numbers.map(n => n * 2);
+    return doubled
+};
+
+const result2 = doubleArray([1, 2, 3]);
+
+const scream = function (sentence) {
+    console.log(`${sentence}!!!`);
+};
+const result3 = scream("Bees");
+console.log(result3);
