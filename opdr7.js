0e960e1091e04be60eb0409233d93ffb1fd1feda
YSNSVC <89729328+YSNSVC@users.noreply.github.com>
2021-11-14 15:25:54 +0100

    Update opdr7.js

--- opdr7.js
+++ opdr7.js
@@ -1,24 +1,33 @@
-const add = function () {
-    const sum = number1 + number2;
-    return sum;
-};
-return true;
-
-};
-
-const number1 = 50;
-console.log(result);
-
-
-const doubleArray = function (numbers) {
-    const doubled = numbers.map(n => n * 2);
-    return doubled
+const numberIsBig = function (number) {
+    if (number > 100) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
-const result2 = doubleArray([1, 2, 3]);
+console.log(numberIsBig(150));
+console.log(numberIsBig(50));
+console.log(numberIsBig(0));
+console.log(numberIsBig(999));
 
-const scream = function (sentence) {
-    console.log(`${sentence}!!!`);
+const getBouncerReaction = function (
+    maxVisitors,
+    currentVisitors,
+    ageOfPotentialVisitor
+) {
+    if (ageOfPotentialVisitor < 18) {
+        return "this is a club for adults";
+    }
+
+    if (currentVisitors >= maxVisitors) {
+        return "it's too busy now, come back later";
+    } else {
+        return "come in";
+    }
 };
-const result3 = scream("Bees");
-console.log(result3);
+
+console.log(getBouncerReaction(2000, 0, 15)); // "this is a club for adults"
+console.log(getBouncerReaction(2000, 1999, 50)); // "come in"
+console.log(getBouncerReaction(2000, 2000, 40)); // "it's too busy now, come back later"
+console.log(getBouncerReaction(2000, 2999, 30)); // "it's too busy now, come back later"
